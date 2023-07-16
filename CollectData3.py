9b7126a566713227a12d0f2dd6e978aaa52e5dad
Daniel Knudsen <daniel.d.knudsen@gmail.com>
2023-07-16 15:01:49 +0200

    alter path afain

--- CollectData3.py
+++ CollectData3.py
@@ -146,7 +146,7 @@ for indexx in [1]:
     break
 
 import os
-dataset_name = f"dataset_{len(os.listdir('./datasets/'))}.pkl"
+dataset_name = f"dataset_{len(os.listdir('/home/daniel_d_knudsen/csgo-market-analysis/datasets/'))}.pkl"
 
-with open("./datasets/"+dataset_name, 'wb') as handle:
+with open("/home/daniel_d_knudsen/csgo-market-analysis/datasets/"+dataset_name, 'wb') as handle:
     pickle.dump(dataset, handle)
